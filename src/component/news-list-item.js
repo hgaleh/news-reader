776ff7210b2e258f14ac6b4923c71d0f6c39e2f8
Kiya <h.galeh@gmail.com>
2019-03-19 11:55:12 +0330

    bootstrap

--- src/component/news-list-item.js
+++ src/component/news-list-item.js
@@ -9,7 +9,7 @@ class NewsListItem extends Component {
 	render() {
 		const {item} = this.props;
 		return (
-			<div className="card" style={{width:'250px', maxHeight: '400px', display: 'inline-block', marginRight: '10px'}}>
+			<div className="card" style={{width:'250px', display: 'inline-block', marginRight: '10px'}}>
 				<img className="card-img-top" src={item.urlToImage} alt={item.title} style={{width:'100%'}}/>
 				<div className="card-body">
 					<h4 className="card-title">{item.author}</h4>
